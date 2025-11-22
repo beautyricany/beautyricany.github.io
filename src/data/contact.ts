6e5153a8d5f5d935ef9720764797e380b41afcda
Katerina Prusova <provkateina@microsoft.com>
2025-11-22 19:46:48 +0100

    footer fixed

---
+++ src/data/contact.ts
@@ -0,0 +1,19 @@
+export interface Contact {
+    ownerName: string;
+    ownerIco: string;
+    address: string;
+    email: string;
+    phone: string;
+    facebook: string;
+    instagram: string;
+}
+
+export const contact: Contact = {
+    ownerName: "Eva Průšová",
+    ownerIco: "12345678",
+    address: "Placeholder Street 123, 100 00 Prague, Czech Republic",
+    email: "info@permanentbeauty.cz",
+    phone: "+420 123 456 789",
+    facebook: "https://facebook.com/permanentbeauty",
+    instagram: "https://instagram.com/permanentbeauty"
+};
